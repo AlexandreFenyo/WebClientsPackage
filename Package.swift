c993393906900f9167b6e8b60fd6f228eafd2330
Alexandre Fenyo <github@fenyo.net>
2024-09-23 20:03:04 +0200

    update

--- Package.swift
+++ Package.swift
@@ -1,37 +1,3 @@
-// swift-tools-version:6.0
-
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-import PackageDescription
-
-let package = Package(
-    name: "WebClientsPackage",
-    platforms: [
-            .macOS(.v13),
-            .iOS(.v16),
-            .watchOS(.v7),
-            .tvOS(.v13)
-        ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "WebClientsPackage",
-            targets: ["WebClientsPackage"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "WebClientsPackage"),
-        .testTarget(
-            name: "WebClientsPackageTests",
-            dependencies: ["WebClientsPackage"]
-        ),
-    ]
-)
-
-/*
- Pour playgrounds Swift :
  // swift-tools-version:5.3
 
  // The swift-tools-version declares the minimum version of Swift required to build this package.
@@ -63,4 +29,4 @@ let package = Package(
          ),
      ]
  )
- */
+ 
