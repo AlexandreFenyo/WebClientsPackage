31b61f3d6316ec16df5ec76746b26ec5656a686a
Alexandre Fenyo <github@fenyo.net>
2024-09-25 00:50:30 +0200

    update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7
 
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
@@ -6,12 +6,12 @@ import PackageDescription
 
 let package = Package(
     name: "WebClientsPackage",
+
     platforms: [
             .macOS(.v13),
             .iOS(.v16),
-            .watchOS(.v7),
-            .tvOS(.v13)
         ],
+
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -29,38 +29,3 @@ let package = Package(
         ),
     ]
 )
-
-/*
- Pour playgrounds Swift :
- // swift-tools-version:5.3
-
- // The swift-tools-version declares the minimum version of Swift required to build this package.
-
- import PackageDescription
-
- let package = Package(
-     name: "WebClientsPackage",
- //    platforms: [
- //        .macOS(.v13),
- //            .iOS(.v16)//,
- //            .watchOS(.v7),
- //            .tvOS(.v13)
- //        ],
-     products: [
-         // Products define the executables and libraries a package produces, making them visible to other packages.
-         .library(
-             name: "WebClientsPackage",
-             targets: ["WebClientsPackage"]),
-     ],
-     targets: [
-         // Targets are the basic building blocks of a package, defining a module or a test suite.
-         // Targets can depend on other targets in this package and products from dependencies.
-         .target(
-             name: "WebClientsPackage"),
-         .testTarget(
-             name: "WebClientsPackageTests",
-             dependencies: ["WebClientsPackage"]
-         ),
-     ]
- )
- 
